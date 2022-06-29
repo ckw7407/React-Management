bb534177db5a71fbae86b46b2fe9f1133f74335c
kw cho <ckw7407@naver.com>
2022-06-29 13:37:41 +0900

    modify App.js state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import Subject from './components/Subject';
 import Content from './components/Content';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB',sub:'World Wid Web'},
+      contents:[
+        {id:1,title:'HTML',desc:'HTMO is for imformation'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render(){
     return (
       <div>
-        <Subject title="WEB" sub="wordl wide web!"></Subject>  {/* <Subject /> */}
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>  {/* <Subject /> */}
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,19 @@ import React,{ Component } from 'react';
 
 class TOC extends Component{
     render(){
+      let lists = [];
+      let data = this.props.data;
+      let i = 0;
+
+      while(i < data.length){
+        lists.push(<li><a href={"/content/" + data[i].id}>{data[i].title}</a></li>); 
+        i = i + 1;
+      }
+      
       return(
         <nav>
           <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
+              {lists}
           </ul>
         </nav>
       );
