20055d736404eab5c49253cb918afe51280ca885
kw cho <ckw7407@naver.com>
2022-06-29 10:27:38 +0900

    create react management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{ Component } from 'react';
+import TOC from './components/TOC'
+import Subject from './components/Subject';
+import Content from './components/Content';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Subject title="WEB" sub="wordl wide web!"></Subject>  {/* <Subject /> */}
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Subject.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+
+class Subject extends Component{
+    render(){
+      return(
+        <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+        </header>
+      );
+    }
+  }
+
+  export default Subject;
