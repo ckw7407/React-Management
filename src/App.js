2bcd54bb97f1b6e962a200b8623f4a450d8255f6
kw cho <ckw7407@naver.com>
2022-07-01 10:38:51 +0900

    create delete

--- src/App.js
+++ src/App.js
@@ -125,9 +125,27 @@ class App extends Component{
         >
         </TOC>
         <Control onChangeMode={function(_mode){
-            this.setState({
-              mode:_mode
-            });
+            if(_mode === 'delete') {
+              if(window.confirm('really?')){
+                let _contents = Array.from(this.state.contents);
+                let i = 0;
+                while(i < _contents.length){
+                  if(_contents[i].id === this.state.selected_content_id){
+                    _contents.splice(i,1);
+                    break;
+                  }
+                  i = i + 1;
+                }
+                this.setState({
+                  mode:'welcome',
+                  contents:_contents
+                })
+              }
+            }else{
+              this.setState({
+                mode:_mode
+              });
+            } 
           }.bind(this)}></Control>
         {this.getContent()}
       </div>
